e08aa1240dc6a07bd35e454ca9c8a63f22df9be1
pomelo072 <2641316287@qq.com>
2020-05-28 22:41:16 +0800

    Update 5.28

--- LiZixia/project/demo/polls/models.py
+++ LiZixia/project/demo/polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -7,6 +8,7 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
